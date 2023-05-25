bf0d55598be04b453c7de08fe6f8dc5d65cb4bf3
hugopatiñooo19 <133272436+gfgdgdfgfgsg@users.noreply.github.com>
2023-05-25 14:35:20 -0600

    funcionalidad 3

--- finalLenguaje/src/models/principal.java
+++ finalLenguaje/src/models/principal.java
@@ -16,15 +16,7 @@ public class principal {
 
         clase.add(new Clases(cantHoja, tempoPicad, arom, azuca, nicotin));
 
-        int total = 0;
-        for (int i = 0; i < clase.size(); i++) {
-            total += clase.get(i).getCantHojas();
-            total += clase.get(i).getTempoPicado();
-            total += clase.get(i).getAroma();
-            total += clase.get(i).getAzucar();
-            total += clase.get(i).getNicotina();
-        }
-        System.out.println(total);
+        System.out.println(clase.toString());
 
         picadoDesvenado();
 
@@ -38,7 +30,22 @@ public class principal {
             }
             System.out.println("Picado y desvenado " + i);
         }
+
+        cantidadCigarros();
     }
 
+    //Realice un método para su clase que obtenga la cantidad de cigarrillos que obtuvo del proceso.
+    public static void cantidadCigarros() {
+
+        int total = 0;
+        for (int i = 0; i < clase.size(); i++) {
+            total += clase.get(i).getCantHojas();
+            total += clase.get(i).getTempoPicado();
+            total += clase.get(i).getAroma();
+            total += clase.get(i).getAzucar();
+            total += clase.get(i).getNicotina();
+        }
+        System.out.println(total);
+    }
 
 }
